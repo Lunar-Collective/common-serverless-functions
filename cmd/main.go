c79f7686c440c66cd11c2eeb6094bc9b8d329314
stephen miracle <miracle@stephens-MBP.attlocal.net>
2020-07-31 04:20:05 -0400

    updating for graphql to fit within lambda.

--- cmd/main.go
+++ cmd/main.go
@@ -2,37 +2,35 @@ package main
 
 import (
 	"context"
-	gqlhandler "github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/99designs/gqlgen/handler"
 	"github.com/Lunar-Collective/go-graphql-lambda/graph"
 	"github.com/Lunar-Collective/go-graphql-lambda/graph/generated"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/awslabs/aws-lambda-go-api-proxy/gorillamux"
+	"github.com/awslabs/aws-lambda-go-api-proxy/handlerfunc"
 	"github.com/gorilla/mux"
 	"log"
 )
 
-var muxAdapter *gorillamux.GorillaMuxAdapter
+var apiGatewayAdapter *handlerfunc.HandlerFuncAdapter
 
 func init() {
-	r := mux.NewRouter()
-
-	// From server.go
-	schema := generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}})
-	server := gqlhandler.NewDefaultServer(schema)
-	r.Handle("/query", server)
-	r.Handle("/", playground.Handler("GraphQL playground", "/query"))
-
-	muxAdapter = gorillamux.New(r)
+	schema := generated.NewExecutableSchema(generated.Config{
+		Resolvers: &graph.Resolver{},
+	})
+	gqlHandler := handler.GraphQL(schema)
+	apiGatewayAdapter = handlerfunc.New(gqlHandler)
 }
 
 func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	rsp, err := muxAdapter.Proxy(req)
+	rsp, err := apiGatewayAdapter.ProxyWithContext(ctx, req)
 
 	if err != nil {
 		log.Println(err)
 	}
+
 	return rsp, err
 }
 
