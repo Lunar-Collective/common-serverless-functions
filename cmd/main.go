54331cb43d349f1bf4ef05c9d3bba97f1f38271c
stephen miracle <miracle@stephens-MBP.attlocal.net>
2020-07-31 04:11:50 -0400

    adding gorilla mux to handler to be able to run gql

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,42 @@
 package main
 
 import (
+	"context"
+	gqlhandler "github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/Lunar-Collective/go-graphql-lambda/graph"
+	"github.com/Lunar-Collective/go-graphql-lambda/graph/generated"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/gorillamux"
+	"github.com/gorilla/mux"
+	"log"
 )
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       "Hello AWS Lambda and Netlify",
-	}, nil
+var muxAdapter *gorillamux.GorillaMuxAdapter
+
+func init() {
+	r := mux.NewRouter()
+
+	// From server.go
+	schema := generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}})
+	server := gqlhandler.NewDefaultServer(schema)
+	r.Handle("/query", server)
+	r.Handle("/", playground.Handler("GraphQL playground", "/query"))
+
+	muxAdapter = gorillamux.New(r)
+}
+
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	rsp, err := muxAdapter.Proxy(req)
+
+	if err != nil {
+		log.Println(err)
+	}
+	return rsp, err
 }
 
 func main() {
 	// Make the handler available for Remote Procedure Call by AWS Lambda
-	lambda.Start(handler)
+	lambda.Start(Handler)
 }
